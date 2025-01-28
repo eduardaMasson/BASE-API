9233d3c1175924708c29ed2cd7e30c7b15313608
eduardaMasson <167575487+eduardaMasson@users.noreply.github.com>
2025-01-28 17:17:16 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'CHAVE_SECRETA_SECRETISSIMA'
+DEBUG = True
+
+#conexao do banco
+DB_HOST = 'localhost'
+DB_NAME = r'C:\Users\Aluno\Downloads\BANCO (2)\BANCO.FDB'
+DB_USER = 'sysdba'
+DB_PASSWORD = 'sysdba'
