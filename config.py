2ef340c184fa7104fa3ba5a1b93147629952c977
eduardaMasson <167575487+eduardaMasson@users.noreply.github.com>
2025-02-04 17:18:42 -0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'CHAVE_SECRETA_SECRETISSIMA'
-DEBUG = True
-
-#conexao do banco
-DB_HOST = 'localhost'
-DB_NAME = r'C:\Users\Aluno\Downloads\BANCO (2)\BANCO.FDB'
-DB_USER = 'sysdba'
-DB_PASSWORD = 'sysdba'
